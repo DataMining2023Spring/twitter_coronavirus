07650ed294cba451751112cd8c8dc48d6c33bd5e
Daniel Monzon <monzondaniel1@gmail.com>
2023-03-04 16:17:01 -0800

    new

--- src/visualize.py
+++ src/visualize.py
@@ -34,7 +34,7 @@ for k,v in items:
     print(k,':',v)
 
 # create bar graph
-graphItems = sorted(items[:10], key=lambda x: x[0])
+graphItems = sorted(items[:10], key=lambda x: x[1])
         #[:10] returns first ten items
 
 graph_x, graph_y = [*zip(*graphItems)]
